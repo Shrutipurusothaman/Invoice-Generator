12f1698ee82c173660f02a2f705360a654fd9b24
Shrutipurusothaman <shrutikishore04@gmail.com>
2026-01-29 13:24:42 +0530

    started with upload and title section of the invoice

---
+++ assests/script.js
@@ -0,0 +1,6 @@
+//sign-up button 
+const signUp_button=document.getElementById('Signup-Button');
+signUp_button.addEventListener('click',()=>{
+    alert
+    ("Please sign up to process the invoice details");
+});
