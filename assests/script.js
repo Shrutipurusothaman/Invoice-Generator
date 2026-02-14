1f7f00a5a85c6a9fb46090c5ac4088330ed02f04
Shrutipurusothaman <shrutikishore04@gmail.com>
2026-02-14 21:56:51 +0530

    completed till place of supply

--- assests/script.js
+++ assests/script.js
@@ -1,6 +1,5 @@
 //sign-up button 
 const signUp_button=document.getElementById('Signup-Button');
 signUp_button.addEventListener('click',()=>{
-    alert
-    ("Please sign up to process the invoice details");
+    alert("Please sign up to process the invoice details");
 });
